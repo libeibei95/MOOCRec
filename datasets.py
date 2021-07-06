ad032ac804ca240f06e1d4c9be50c835f76df3af
Abinash <sinha160@umn.edu>
2021-07-06 05:36:27 -0500

    typcast str to int for each item
    typcast str to int for each item

--- datasets.py
+++ datasets.py
@@ -1,153 +1,327 @@
 """
-Script used for preparing dataset for pre-training and fine-tuning the model
+Script having utility functions usable by multiples scripts
+in the project
 
 @author: Abinash Sinha
 """
 
+import numpy as np
+import pandas as pd
+import math
+import random
+import os
+from scipy.sparse import csr_matrix
+
 import torch
-from torch.utils.data import Dataset
-
-from utils import neg_sample
 
 
-class PretrainDataset(Dataset):
+def set_seed(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    # some cudnn methods can be random even after fixing the seed
+    # unless you tell it to be deterministic
+    torch.backends.cudnn.deterministic = True
 
-    def __init__(self, args, user_seq):
-        self.args = args
-        self.user_seq = user_seq
-        self.max_len = args.max_seq_length
-        self.part_sequence = []
-        self.split_sequence()
 
-    def split_sequence(self):
-        for seq in self.user_seq:
-            input_ids = seq[-(self.max_len + 2):-2]  # keeping same as train set
-            for i in range(len(input_ids)):
-                self.part_sequence.append(input_ids[:(i+1) + 1])
+def check_path(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print(f'{path} created')
 
-    def __len__(self):
-        return len(self.part_sequence)
 
-    def __getitem__(self, index):
+def neg_sample(item_set, item_size):  # 前闭后闭
+    item = random.randint(1, item_size - 1)
+    while item in item_set:
+        item = random.randint(1, item_size - 1)
+    return item
+
+
+class EarlyStopping:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+
+    def __init__(self, checkpoint_path, patience=7, verbose=False, delta=0):
         """
-
-        :param index:
-        :return:
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement.
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
         """
-        # (all possible sequences made using all possible subsets of sequences)
-        sequence = self.part_sequence[index]  # pos_items
+        self.checkpoint_path = checkpoint_path
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.delta = delta
 
-        seq_len = len(sequence)
-        if seq_len == 2:
-            t = 1
+    def compare(self, score):
+        for i in range(len(score)):
+            # 有一个指标增加了就认为是还在涨
+            if score[i] > self.best_score[i] + self.delta:
+                return False
+        return True
+
+    def __call__(self, score, model):
+        # score HIT@10 NDCG@10
+
+        if self.best_score is None:
+            self.best_score = score
+            self.score_min = np.array([0] * len(score))
+            self.save_checkpoint(score, model)
+        elif self.compare(score):
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
         else:
-            t = torch.randint(1, seq_len-1, (1,))
-        # input sub-sequence
-        inp_subseq = sequence[:t]
-        inp_pad_len = self.max_len - len(inp_subseq)
-        inp_pos_items = ([0] * inp_pad_len) + inp_subseq
-        inp_pos_items = inp_pos_items[-self.max_len:]
+            self.best_score = score
+            self.save_checkpoint(score, model)
+            self.counter = 0
 
-        # label sub-sequence
-        label_subseq = sequence[t:]
-        label_pad_len = self.max_len - len(label_subseq)
-        label_pos_items = [0] * label_pad_len + label_subseq
-        label_pos_items = label_pos_items[-self.max_len:]
-        label_pos_items.reverse()
-        # next item
-        next_item = [sequence[t]]
-
-        assert len(inp_pos_items) == self.max_len
-        assert len(label_pos_items) == self.max_len
-
-        cur_tensors = (
-            torch.tensor(inp_pos_items, dtype=torch.long),  # actual input sub-sequence of items
-            torch.tensor(label_pos_items, dtype=torch.long),  # actual label sub-sequence of items
-            torch.tensor(next_item, dtype=torch.long)  # item next to input sub-sequence of items
-        )
-
-        return cur_tensors
+    def save_checkpoint(self, score, model):
+        '''Saves model when validation loss decrease.'''
+        if self.verbose:
+            # ({self.score_min:.6f} --> {score:.6f}) # 这里如果是一个值的话输出才不会有问题
+            print(f'Validation score increased.  Saving model ...')
+        torch.save(model.state_dict(), self.checkpoint_path)
+        self.score_min = score
 
 
-class FineTrainDataset(Dataset):
+def generate_rating_matrix_valid(user_seq, num_users, num_items):
+    # three lists are used to construct sparse matrix
+    row = []
+    col = []
+    data = []
+    for user_id, item_list in enumerate(user_seq):
+        for item in item_list[:-2]:  #
+            row.append(user_id)
+            col.append(item)
+            data.append(1)
 
-    def __init__(self, args, user_seq, test_neg_items=None, data_type='train'):
-        self.args = args
-        self.user_seq = user_seq
-        self.test_neg_items = test_neg_items
-        self.data_type = data_type
-        self.max_len = args.max_seq_length
+    row = np.array(row)
+    col = np.array(col)
+    data = np.array(data)
+    rating_matrix = csr_matrix((data, (row, col)), shape=(num_users, num_items))
 
-    def __getitem__(self, index):
+    return rating_matrix
 
-        user_id = index
-        items = self.user_seq[index]
 
-        assert self.data_type in {"train", "valid", "test"}
+def generate_rating_matrix_test(user_seq, num_users, num_items):
+    # three lists are used to construct sparse matrix
+    row = []
+    col = []
+    data = []
+    for user_id, item_list in enumerate(user_seq):
+        for item in item_list[:-1]:  #
+            row.append(user_id)
+            col.append(item)
+            data.append(1)
 
-        # [0, 1, 2, 3, 4, 5, 6]
-        # train [0, 1, 2, 3]
-        # target [1, 2, 3, 4]
+    row = np.array(row)
+    col = np.array(col)
+    data = np.array(data)
+    rating_matrix = csr_matrix((data, (row, col)), shape=(num_users, num_items))
 
-        # valid [0, 1, 2, 3, 4]
-        # answer [5]
+    return rating_matrix
 
-        # test [0, 1, 2, 3, 4, 5]
-        # answer [6]
-        if self.data_type == "train":
-            input_ids = items[:-3]
-            target_pos = items[1:-2]
-            answer = [0]  # no use
 
-        elif self.data_type == 'valid':
-            input_ids = items[:-2]
-            target_pos = items[1:-1]
-            answer = [items[-2]]
+def get_user_seqs_csv(data_file):
+    data_df = pd.read_csv(data_file)
+    user_seq = []
+    item_set = set()
+    user_set = set()
+    for _, row in data_df.iterrows():
+        items = row['video_ids'].split(',')
+        user_seq.append(items)
+        item_set = item_set | set(items)
+        user_set.add(row['id'])
 
-        else:
-            input_ids = items[:-1]
-            target_pos = items[1:]
-            answer = [items[-1]]
+    num_users = len(user_set)
+    max_item = len(item_set)
+    num_items = max_item + 2
+    assert len(user_set) == len(user_seq)
 
-        target_neg = []
-        seq_set = set(items)
-        for _ in input_ids:
-            target_neg.append(neg_sample(seq_set, self.args.item_size))
+    valid_rating_matrix = generate_rating_matrix_valid(user_seq, num_users, num_items)
+    test_rating_matrix = generate_rating_matrix_test(user_seq, num_users, num_items)
+    return user_seq, max_item, valid_rating_matrix, test_rating_matrix
 
-        pad_len = self.max_len - len(input_ids)
-        input_ids = [0] * pad_len + input_ids
-        target_pos = [0] * pad_len + target_pos
-        target_neg = [0] * pad_len + target_neg
 
-        input_ids = input_ids[-self.max_len:]
-        target_pos = target_pos[-self.max_len:]
-        target_neg = target_neg[-self.max_len:]
+def get_user_seqs_long_csv(data_file):
+    """
 
-        assert len(input_ids) == self.max_len
-        assert len(target_pos) == self.max_len
-        assert len(target_neg) == self.max_len
+    :param data_file:
+    :return:
+    user_seq:
+        list of item sequences
+    max_item:
+        item with largest item id (number of items basically)
 
-        if self.test_neg_items is not None:
-            test_samples = self.test_neg_items[index]
+    """
+    data_df = pd.read_csv(data_file)
+    user_seq = []
+    item_set = set()
+    user_set = set()
+    for idx, row in data_df.iterrows():
+        items = row['video_ids'].split(',')
+        items = [int(item) for item in items]
+        user_seq.append(items)
+        item_set = item_set | set(items)
+        user_set.add(row['id'])
+    max_item = len(item_set)
+    assert len(user_set) == len(user_seq)
+    return user_seq, max_item
 
-            cur_tensors = (
-                torch.tensor(user_id, dtype=torch.long),  # user_id for testing
-                torch.tensor(input_ids, dtype=torch.long),
-                torch.tensor(target_pos, dtype=torch.long),
-                torch.tensor(target_neg, dtype=torch.long),
-                torch.tensor(answer, dtype=torch.long),
-                torch.tensor(test_samples, dtype=torch.long),
-            )
-        else:
-            cur_tensors = (
-                torch.tensor(user_id, dtype=torch.long),  # user_id for testing
-                torch.tensor(input_ids, dtype=torch.long),
-                torch.tensor(target_pos, dtype=torch.long),
-                torch.tensor(target_neg, dtype=torch.long),
-                torch.tensor(answer, dtype=torch.long),
-            )
 
-        return cur_tensors
+def get_user_seqs_and_sample(data_file, sample_file):
+    data_df = pd.read_csv(data_file)
+    user_seq = []
+    item_set = set()
+    for _, row in data_df.iterrows():
+        items = row['video_ids'].split(',')
+        items = [int(item) for item in items]
+        user_seq.append(items)
+        item_set = item_set | set(items)
 
-    def __len__(self):
-        return len(self.user_seq)
+    max_item = len(item_set)
+
+    sample_df = pd.read_csv(sample_file)
+    sample_seq = []
+    for _, row in sample_df.iterrows():
+        items = row['video_ids'].split(',')
+        items = [int(item) for item in items]
+        sample_seq.append(items)
+        item_set = item_set | set(items)
+
+    assert len(user_seq) == len(sample_seq)
+
+    return user_seq, max_item, sample_seq
+
+
+def get_metric(pred_list, topk=10):
+    NDCG = 0.0
+    HIT = 0.0
+    MRR = 0.0
+    # [batch] the answer's rank
+    for rank in pred_list:
+        MRR += 1.0 / (rank + 1.0)
+        if rank < topk:
+            NDCG += 1.0 / np.log2(rank + 2.0)
+            HIT += 1.0
+    return HIT / len(pred_list), NDCG / len(pred_list), MRR / len(pred_list)
+
+
+def precision_at_k_per_sample(actual, predicted, topk):
+    num_hits = 0
+    for place in predicted:
+        if place in actual:
+            num_hits += 1
+    return num_hits / (topk + 0.0)
+
+
+def precision_at_k(actual, predicted, topk):
+    sum_precision = 0.0
+    num_users = len(predicted)
+    for i in range(num_users):
+        act_set = set(actual[i])
+        pred_set = set(predicted[i][:topk])
+        sum_precision += len(act_set & pred_set) / float(topk)
+
+    return sum_precision / num_users
+
+
+def recall_at_k(actual, predicted, topk):
+    sum_recall = 0.0
+    num_users = len(predicted)
+    true_users = 0
+    for i in range(num_users):
+        act_set = set(actual[i])
+        pred_set = set(predicted[i][:topk])
+        if len(act_set) != 0:
+            sum_recall += len(act_set & pred_set) / float(len(act_set))
+            true_users += 1
+    return sum_recall / true_users
+
+
+def apk(actual, predicted, k=10):
+    """
+    Computes the average precision at k.
+    This function computes the average precision at k between two lists of
+    items.
+    Parameters
+    ----------
+    actual : list
+             A list of elements that are to be predicted (order doesn't matter)
+    predicted : list
+                A list of predicted elements (order does matter)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The average precision at k over the input lists
+    """
+    if len(predicted) > k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    for i, p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i + 1.0)
+
+    if not actual:
+        return 0.0
+
+    return score / min(len(actual), k)
+
+
+def mapk(actual, predicted, k=10):
+    """
+    Computes the mean average precision at k.
+    This function computes the mean average prescision at k between two lists
+    of lists of items.
+    Parameters
+    ----------
+    actual : list
+             A list of lists of elements that are to be predicted
+             (order doesn't matter in the lists)
+    predicted : list
+                A list of lists of predicted elements
+                (order matters in the lists)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The mean average precision at k over the input lists
+    """
+    return np.mean([apk(a, p, k) for a, p in zip(actual, predicted)])
+
+
+def ndcg_k(actual, predicted, topk):
+    res = 0
+    for user_id in range(len(actual)):
+        k = min(topk, len(actual[user_id]))
+        idcg = idcg_k(k)
+        dcg_k = sum([int(predicted[user_id][j] in
+                         set(actual[user_id])) / math.log(j + 2, 2) for j in range(topk)])
+        res += dcg_k / idcg
+    return res / float(len(actual))
+
+
+# Calculates the ideal discounted cumulative gain at k
+def idcg_k(k):
+    res = sum([1.0 / math.log(i + 2, 2) for i in range(k)])
+    if not res:
+        return 1.0
+    else:
+        return res
