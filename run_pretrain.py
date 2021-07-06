f1cd1462f46770f36fc14f805ba73b18fdbee720
Abinash <sinha160@umn.edu>
2021-07-06 03:19:56 -0500

    pre-train checkpoint code added
    save checkpoint after each epoch and resume from the last pre-trained checkpoint

--- run_pretrain.py
+++ run_pretrain.py
@@ -30,7 +30,7 @@ def main():
 
     parser.add_argument("--hidden_size", type=int, default=64, help="hidden size of transformer model")
     parser.add_argument("--num_hidden_layers", type=int, default=2, help="number of layers")
-    parser.add_argument('--num_attention_heads', default=2, type=int)
+    parser.add_argument('--num_attention_heads', default=1, type=int)
     parser.add_argument('--hidden_act', default="gelu", type=str)  # gelu relu
     parser.add_argument("--attention_probs_dropout_prob", type=float, default=0.5, help="attention dropout p")
     parser.add_argument("--hidden_dropout_prob", type=float, default=0.5, help="hidden dropout p")
@@ -41,7 +41,7 @@ def main():
 
     # train args
     parser.add_argument("--lr", type=float, default=0.001, help="learning rate of adam")
-    parser.add_argument("--batch_size", type=int, default=256, help="number of batch_size")
+    parser.add_argument("--batch_size", type=int, default=128, help="number of batch_size")
     parser.add_argument("--epochs", type=int, default=200, help="number of epochs")
     parser.add_argument("--no_cuda", action="store_true")
     parser.add_argument("--log_freq", type=int, default=1, help="per epoch print res")
@@ -50,6 +50,7 @@ def main():
     # pre train args
     parser.add_argument("--pre_epochs", type=int, default=300, help="number of pre_train epochs")
     parser.add_argument("--pre_batch_size", type=int, default=100)
+    parser.add_argument('--ckp', default=20, type=int, help="pretrain epochs 10, 20, 30...")
 
     parser.add_argument("--mask_p", type=float, default=0.2, help="mask probability")
     parser.add_argument("--s2i_weight", type=float, default=1.0, help="seq2item loss weight")
@@ -70,7 +71,11 @@ def main():
 
     args.data_file = os.path.join(args.data_dir, args.data_name + '.csv')
 
-    user_seq, max_item, long_sequence = get_user_seqs_long_csv(args.data_file)
+    user_seq, max_item = get_user_seqs_long_csv(args.data_file)
+    # args.ckp = 20
+    args_str = f'{args.model_name}-{args.data_name}-epochs-{args.ckp}'
+    checkpoint = args_str + '.pt'
+    args.checkpoint_path = os.path.join(args.output_dir, checkpoint)
 
     args.item_size = max_item + 2
     args.mask_id = max_item + 1
@@ -84,18 +89,26 @@ def main():
     model = EduRecModel(args=args)
     trainer = PretrainTrainer(model, None, None, None, args)
 
+    # to resume training from last trained epoch
+    if os.path.exists(args.checkpoint_path):
+        trainer.load(args.checkpoint_path)
+        print(f'Resume training from epoch={args.ckp} for pre-training!')
+        init_epoch = int(args.ckp)
+    else:
+        init_epoch = -1
     for epoch in range(args.pre_epochs):
-
+        if epoch <= init_epoch:
+          continue
         pretrain_dataset = PretrainDataset(args, user_seq)
         pretrain_sampler = RandomSampler(pretrain_dataset)
         pretrain_dataloader = DataLoader(pretrain_dataset, sampler=pretrain_sampler, batch_size=args.pre_batch_size)
 
         trainer.pretrain(epoch, pretrain_dataloader)
 
-        if (epoch + 1) % 10 == 0:
-            ckp = f'{args.data_name}-epochs-{epoch+1}.pt'
-            checkpoint_path = os.path.join(args.output_dir, ckp)
-            trainer.save(checkpoint_path)
+        # if (epoch + 1) % 10 == 0:
+        ckp = f'{args.model_name}-{args.data_name}-epochs-{epoch+1}.pt'
+        checkpoint_path = os.path.join(args.output_dir, ckp)
+        trainer.save(checkpoint_path)
 
 
 main()
